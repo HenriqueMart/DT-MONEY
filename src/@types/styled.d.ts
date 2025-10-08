a40ad9aa1903f778d778b6348a0ae54f632e25d0
henriquemart <h.martins.silva2014@gmail.com>
2025-10-08 20:27:57 -0300

    Started: Struct projetc

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,10 @@
+//Arquivo de Tipagem para o Styled Components
+
+import 'styled-components';
+import type { defaultTheme } from '../styles/themes/default';
+
+type ThemeType = typeof defaultTheme
+
+declare module 'styled-components' {
+    export interface  DefaultTheme extends ThemeType {}
+}
